57fa89b36b704e4adaf01bdf08e16789be786a53
suikhan@126.com <suikhan@126.com>
2018-10-18 22:37:49 +0800

    a

--- oracle/ora.go
+++ oracle/ora.go
@@ -1,28 +1,34 @@
 package main
 
-import(
-    "database/sql"
-    "log"
-    _ "github.com/mattn/go-oci8"
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-oci8"
 )
 
-func main(){
-    db, err := sql.Open("oci8", "his_gyyy/hiscss@orcl")
-    if err != nil{
-        log.Fatal(err)
-    }
-    rows, err := db.Query("select * from users")
-    defer rows.Close()
-    if err != nil{
-        log.Fatal(err)
-    }
-    for rows.Next(){
-        var i string
-        err = rows.Scan(&i)
-        if err != nil{
-            log.Fatal(err)
-        }
-        println(i)
-    }
-}
+func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.Println("OracleDriver example")
+	os.Setenv("NLS_LANG", "")
 
+	db, err := sql.Open("oci8", "his_gyyy/hiscss@orcl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query("select * from users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var i string
+		err = rows.Scan(&i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		println(i)
+	}
+	rows.Close()
+	db.Close()
+}
