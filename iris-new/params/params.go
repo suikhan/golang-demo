0d102f607c52a079af1b4a78440dd6a6cacb287d
suikhan@126.com <suikhan@126.com>
2018-11-09 08:56:26 +0800

    第二次培训课后作业

---
+++ iris-new/params/params.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+)
+
+type LoginData struct {
+	username string
+	password string
+}
+
+func Login(ctx iris.Context) {
+	var d LoginData
+	ctx.ReadForm
+	err := ctx.ReadForm(&d)
+	// err := ctx.ReadJSON(&d)
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.WriteString(err.Error())
+		return
+	}
+
+	ctx.WriteString("UserName=" + d.username + ", Password=" + d.password)
+}
+
+func main() {
+	app := iris.New()
+
+	app.Get("/login", Login)
+	app.Post("/login", Login)
+
+	app.Run(iris.Addr(":8080"))
+}
